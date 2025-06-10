4b342422fa91645353428a764d92c6c93e7e0d4f
graMstrs <gra.tavera@gmail.com>
2025-06-10 17:29:07 -0600

    090625_Secc9.74

--- react-guia-definitiva-hooks-router-redux-next-proyectos/forms/src/App.js
+++ react-guia-definitiva-hooks-router-redux-next-proyectos/forms/src/App.js
@@ -1,10 +1,25 @@
 import { useState } from "react"
 
 const App = () => {
-    const [value, setValue] = useState({ normal: '', texto: ''})
-    const handleChange = (e) => {
-        setValue(e.target.value)
-    }
+    const [value, setValue] = 
+        useState({ 
+            normal: 'por defecto', 
+            texto: '', 
+            select: '',
+            check: false,
+            radio: 'feliz'
+    })
+   
+   const handleChange = ({ target }) => {
+      setValue((state) => ({ 
+              ...state,
+              [target.name]: target.type === 'checkbox' 
+                ? target.checked 
+                : target.value
+          })
+      )
+   } 
+    
     console.log(value)
     return (
         <div>
@@ -12,9 +27,56 @@ const App = () => {
             <input 
                 type="text" 
                 name="normal" 
-                value={value} 
+                value={value.normal} 
                 onChange={handleChange}/>
-            <textarea name="texto" onChange={handleChange()} value={''} />
+            
+            <textarea 
+                name="texto"
+                value={value.texto}
+                onChange={handleChange}/>
+
+
+            <select value={value.select} name="select" onChange={handleChange}>
+                <option value=''>-- Seleccione --</option>
+                <option value='chanchoFeliz'>Chancho feliz</option>
+                <option value='chanchitoFeliz'>Chanchito feliz</option>
+                <option value='chanchitoTriste'>Chanchito triste</option>
+                <option value='felipe'>Felipe</option>
+            </select>    
+
+            <input
+                type="checkbox"
+                name="check"
+                onChange={handleChange}
+                checked={value.check}/>
+
+            <div >
+                <label>Chancho</label>
+                <input 
+                    onChange={handleChange} 
+                    type="radio" 
+                    value="feliz" 
+                    name="radio"
+                    checked={value.estado === 'feliz'} 
+                /> Feliz
+
+                <input 
+                    onChange={handleChange} 
+                    type="radio" 
+                    value="triste" 
+                    name="radio"
+                    checked={value.estado === 'triste'} 
+                /> Triste
+
+                <input 
+                    onChange={handleChange} 
+                    type="radio" 
+                    value="felipe" 
+                    name="radio"
+                    checked={value.estado === 'felipe'} 
+                /> Felipe
+            </div>
+
         </div>
     )
 }
