07b2a0e22f3a95b451b8399fbd48291e4cdc1589
graMstrs <gra.tavera@gmail.com>
2025-06-18 16:20:13 -0600

    170625_Secc10.84

---
+++ react-guia-definitiva-hooks-router-redux-next-proyectos/formik/src/App.js
@@ -0,0 +1,46 @@
+import { useFormik } from 'formik'
+
+function App() {
+  const formik = useFormik({
+    initialValues: {
+      name:'',
+      lastName:'',
+      email:'',
+    },
+    onSubmit: values => console.log(values)
+  })
+
+
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <label>Nombre</label>
+      <input
+        name='name'
+        type='text'
+        onChange={formik.handleChange}
+        value={formik.values.name}
+      />
+
+      <label>Apellido</label>
+      <input
+        name='lastname'
+        type='text'
+        onChange={formik.handleChange}
+        value={formik.values.lastName}
+      />
+
+      <label>Correo</label>
+      <input
+        name='email'
+        type='email'
+        onChange={formik.handleChange}
+        value={formik.values.email}
+      />
+
+      <button>Enviar</button>
+
+    </form>
+  );
+}
+
+export default App;
