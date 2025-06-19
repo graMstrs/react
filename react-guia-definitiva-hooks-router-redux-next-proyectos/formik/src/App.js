0b8e6eed9435d87248cf0e07000533ef19a0d2d7
graMstrs <gra.tavera@gmail.com>
2025-06-18 18:02:24 -0600

    180625_Secc10.92

--- react-guia-definitiva-hooks-router-redux-next-proyectos/formik/src/App.js
+++ react-guia-definitiva-hooks-router-redux-next-proyectos/formik/src/App.js
@@ -1,45 +1,56 @@
-import { useFormik } from 'formik'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
+
+const validate = (values) => {
+    const errors ={}
+
+    if(!values.name){
+      errors.name = 'Requerido'
+    } else if (values.name.length < 5){
+      errors.name = 'El nombre es muy corto'
+    }
+    
+    if(!values.lastName){
+      errors.lastName = 'Requerido'
+    } else if (values.lastName.length < 5){
+      errors.lastName = 'El apellido es muy corto'
+    }
+
+    if(!values.email){
+      errors.email = 'Requerido'
+    } else if (values.email.length < 5){
+      errors.email = 'El email es muy corto'
+    }
+
+    return errors
+  }
 
 function App() {
-  const formik = useFormik({
-    initialValues: {
-      name:'',
-      lastName:'',
-      email:'',
-    },
-    onSubmit: values => console.log(values)
-  })
-
-
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <label>Nombre</label>
-      <input
-        name='name'
-        type='text'
-        onChange={formik.handleChange}
-        value={formik.values.name}
-      />
+    <Formik
+        initialValues= {{ name:'', lastName:'', email:'', }}
+        validate = {validate}
+        onSubmit = {values => console.log(values)}
+    >
+        <Form>
+            <TextInput name=''>
+            <label>Nombre</label>
+            <Field name='name' type='text'/>
+            <ErrorMessage name='name'/>
+            <br/>
 
-      <label>Apellido</label>
-      <input
-        name='lastname'
-        type='text'
-        onChange={formik.handleChange}
-        value={formik.values.lastName}
-      />
+            <label>Apellido</label>
+            <Field name='lastName' type='text'/>
+            <ErrorMessage name='lastName'/>
+            <br/>
 
-      <label>Correo</label>
-      <input
-        name='email'
-        type='email'
-        onChange={formik.handleChange}
-        value={formik.values.email}
-      />
+            <label>Correo</label>
+            <Field name='email' type='email'/>
+            <ErrorMessage name='email'/>
+            <br/>
 
-      <button>Enviar</button>
-
-    </form>
+            <button type='submit'>Enviar</button>
+        </Form>
+    </Formik>
   );
 }
 
