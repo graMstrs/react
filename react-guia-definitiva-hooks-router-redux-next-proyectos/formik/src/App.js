9a0588e47eb384c7e6559f6cb287c8382062d9de
graMstrs <gra.tavera@gmail.com>
2025-06-25 16:24:59 -0600

    230625_Secc11.96

--- react-guia-definitiva-hooks-router-redux-next-proyectos/formik/src/App.js
+++ react-guia-definitiva-hooks-router-redux-next-proyectos/formik/src/App.js
@@ -1,4 +1,9 @@
 import { Formik, Form, Field, ErrorMessage } from 'formik'
+import TextInput from './components/TextInput'
+import Checkbox from './components/Checkbox'
+import Select from './components/Select'
+import Radio from './components/Radio'
+
 
 const validate = (values) => {
     const errors ={}
@@ -21,35 +26,53 @@ const validate = (values) => {
       errors.email = 'El email es muy corto'
     }
 
+    if(!values.radio){
+      errors.radio = 'Requerido'
+    }
+
     return errors
   }
 
 function App() {
   return (
     <Formik
-        initialValues= {{ name:'', lastName:'', email:'', }}
+        initialValues= {{ name:'', lastName:'', email:'', chancho:'', radio:'',}}
         validate = {validate}
         onSubmit = {values => console.log(values)}
     >
+        
         <Form>
-            <TextInput name=''>
-            <label>Nombre</label>
-            <Field name='name' type='text'/>
-            <ErrorMessage name='name'/>
+            <TextInput name='name' label='Nombre'/>         
             <br/>
 
-            <label>Apellido</label>
-            <Field name='lastName' type='text'/>
-            <ErrorMessage name='lastName'/>
+            <TextInput name='lastName' label='Apellido'/>  
             <br/>
 
-            <label>Correo</label>
-            <Field name='email' type='email'/>
-            <ErrorMessage name='email'/>
+            <TextInput name='email' label='Correo'/>
+            <br/>
+
+            <Select label='Tipo de chancho' name='chancho'>
+              <option value=''> Seleccione chancho </option>
+              <option value='felipe'> Felipe </option>
+              <option value='chanchitofeliz'> Chanchito feliz </option>
+              <option value='chanchitotriste'> Chanchito triste </option>
+            </Select>
+            <br/>
+
+            <Checkbox name='accept'>
+              Aceptar terminos y condiciones
+            </Checkbox>
+            <br/>
+
+            <Radio name='radio' value='chanchito1' label='chanchito 1'/>
+            <Radio name='radio' value='chanchito2' label='chanchito 2'/>
+            <Radio name='radio' value='chanchito3' label='chanchito 3'/>
+            <ErrorMessage name='radio'/>
             <br/>
 
             <button type='submit'>Enviar</button>
         </Form>
+
     </Formik>
   );
 }
