b529d1cb0a8373daa5066816044e43b0e9c3b5d7
graMstrs <gra.tavera@gmail.com>
2025-06-30 18:06:15 -0600

    300625_Secc12.111

---
+++ react-guia-definitiva-hooks-router-redux-next-proyectos/styled/src/App.js
@@ -0,0 +1,76 @@
+import styled from 'styled-components'
+
+const P = styled.p`
+  font-size:24px;
+  color: red;
+`
+
+const Content = styled.div`
+padding 20px 25px
+`
+
+const Button = styled.button`
+  transition: box-shadow 0.2s ease;
+  background-color: ${props => props.primary ? 'red' : 'white'};
+  color: ${props => props.primary ? 'white' : 'red'};
+  padding: 10px 15px;
+  border: solid 2px red;
+  border-radius: 4px;
+
+  &:hover{
+    box-shadow: 1px 2px 5px rgb(0,0,0,0.3);
+  }
+
+  &.secondary{
+    background-color: blue;
+    border: solid 2px blue;
+  }
+
+  .info{
+    font-size:28px;
+  }
+`
+const BlockButton = styled(Button)`
+width: 100%;
+font-size: 24px;
+`
+
+const Link = ({ className, ...props}) => {
+  return <a className={className} {...props}></a>
+}
+
+const Input = styled.input.attrs(props => ({
+  type: 'text',
+  color: props.color || 'red'
+}))`
+  font-size: 20px;
+  border: 1px solid red;
+  color: ${props => props.color};
+`
+
+const Password = styled(Input).attrs({
+  type:'password'
+})``
+
+
+const StyledLink = styled(Link)`
+  color: blue;
+`
+
+function App() {
+  return (
+    <Content>
+      <P>Hola soy un parrafo!</P>
+      <Button> Enviar <p className="info"> Info </p></Button>
+      <Button primary> Enviar </Button>
+      <Button className='secondary'> Enviar secundario </Button>
+      <BlockButton primary as="a" href='#'> Enviar </BlockButton>
+      <Link>link </Link>
+      <StyledLink>Link con estilo</StyledLink>
+      <Input color='blue' />
+      <Password />
+    </Content>
+  );
+}
+
+export default App;
