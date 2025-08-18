0f1f2990b60b30aef63cc22605e6472930635a33
graMstrs <gra.tavera@gmail.com>
2025-08-18 17:08:51 -0600

    180825_Secc17.158

---
+++ react-guia-definitiva-hooks-router-redux-next-proyectos/redux/src/features/todos.js
@@ -0,0 +1,97 @@
+import { combineReducers } from "redux" 
+import { makeFetchingReducer, makeSetReducer } from "./utils"
+
+
+export const setPending = () => {
+    return { type: 'todos/pending' }
+}
+
+export const setFulfilled = payload => ({  type: 'todosFulfilled', payload})
+
+export const setError = e => ({ type: 'todos/error', error: e.message})
+
+export const setComplete = payload => ({ type: 'todo/complete', payload })
+
+export const setFilter = payload => { type: 'filter/set', payload}
+
+export const fetchThunk = () => async dispatch => {
+    dispatch( setPending() )
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/todos')
+        const data = await response.json()
+        const todos = data.slice(0, 10)
+        dispatch( setFulfilled(todos) )
+    } catch (e) {
+        dispatch( setError())
+    }
+}
+
+export const filterReducer = makeSetReducer(['filter/set'])
+
+export const fetchingReducer = makeFetchingReducer([
+    'todos/pending', 
+    'todos/fulfilled',
+    'todos/rejected',
+])
+
+export const todosReducer = (state = [], action) => {
+    switch (action.type) {
+        case 'todos/fulfilled': {
+            return action.payload
+        }
+        case 'todo/add': {
+            return state.concat({ ...action.payload }) 
+        }
+        case 'todo/complete': {
+            const newTodos = state.map(todo => {
+                if (todo.id === action.payload.id){
+                    return { ...todo, completed: !todo.completed}
+                }
+                return todo
+            })
+        
+            return newTodos
+    
+        }
+        default:
+            return state
+    }
+}
+
+
+export const reducer = combineReducers({
+    todos: combineReducers({
+        entities: todosReducer,
+        status: fetchingReducer
+    }),
+    filter: filterReducer,
+})
+
+
+const selectTodos = state => {
+    const { todos: { entities }, filter } = state
+
+    if (filter === 'complete'){
+        return entities.filter(todo => todo.completed)
+    }
+
+    if (filter === 'incomplete'){
+        return entities.filter(todo => !todo.completed)
+    }
+
+    return entities
+}
+
+const selectStatus = state => state.todos.status
+
+const TodoItem = ({ todo }) => {
+    const dispatch = useDispatch()
+
+    return (
+        <li 
+            style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}
+            onClick={() => dispatch(setComplete(todo))}> 
+                { todo.title }
+        </li>
+    )
+}
---
+++ react-guia-definitiva-hooks-router-redux-next-proyectos/redux/src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { applyMiddleware, configureStore } from '@reduxjs/toolkit'
+import { Provider } from 'react-redux'
+import App from './App';
+import { asyncMiddleware } from './middlewares/async';
+import { reducer } from './features/todos';
+import reportWebVitals from './reportWebVitals';
+
+const store = configureStore({ 
+    reducer: reducer, 
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+})
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <Provider store = {store}>
+
+        <App />
+
+    </Provider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
